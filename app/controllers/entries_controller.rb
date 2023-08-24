436ce1bc08b71d66c856665fa984d8e93f5f27e0
Patrycja Kucharska <patrycja.kucharska@lifen.fr>
2023-08-24 11:37:35 +0200

    save entries in model

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,11 +1,9 @@
 class EntriesController < ApplicationController
 
-  before_action :create_missing_entries, only: [ :index ]
-
   SHOW_DAYS = 7
 
   def index
-    @entries = Entry.where(date: Date.today.iso8601..(Date.today + SHOW_DAYS).iso8601).order(:date)
+    @entries = Entry.get_days(SHOW_DAYS)
   end
 
   def edit
@@ -20,19 +18,4 @@ class EntriesController < ApplicationController
     redirect_to edit_entry_path
   end
 
-  private
-
-  def create_missing_entries
-    SHOW_DAYS.times do |i|
-      date = (Date.today + i).iso8601
-      entries_for_day = Entry.where(date: (Date.today + i).iso8601)
-      unless entries_for_day.size == 2
-        Recipe::MEAL_TYPES.each do |meal_type|
-          if entries_for_day.none? { |o| o.meal_type == meal_type.to_s }
-            Entry.create(date: date, meal_type: meal_type)
-          end
-        end
-      end
-    end
-  end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -6,4 +6,24 @@ class Entry < ApplicationRecord
   def recipe
     Recipe.find recipe_id if recipe_id.present?
   end
+
+  def self.get_days(number_of_days)
+    create_missing_entries(number_of_days)
+    where(date: Date.today.iso8601..(Date.today + number_of_days - 1).iso8601).order(:date)
+  end
+
+  private
+  def self.create_missing_entries(number_of_days)
+    number_of_days.times do |i|
+      date = (Date.today + i).iso8601
+      entries_for_day = where(date: (Date.today + i).iso8601)
+      unless entries_for_day.size == 2
+        Recipe::MEAL_TYPES.each do |meal_type|
+          if entries_for_day.none? { |o| o.meal_type == meal_type.to_s }
+            create(date: date, meal_type: meal_type)
+          end
+        end
+      end
+    end
+  end
 end
