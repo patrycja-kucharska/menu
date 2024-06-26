4254a78da7e66b93565bcd3ec2927ae0d329545f
Patrycja Kucharska <patrycja.kucharska@lifen.fr>
2024-06-26 16:45:38 +0200

    extra

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -4,6 +4,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="form"
 export default class extends Controller {
     initialize() {
+        this.submit = debounce(this.submit.bind(this), 300)
     }
 
     submit() {
